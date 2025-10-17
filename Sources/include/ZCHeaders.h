c0bc20d3914b1cef0db6c7b0903405c00b858cbb
Julien Pouget <julien.pouget@globule.net>
2025-10-17 12:47:08 +0200

    Add modulemap and ZCHeaders files

---
+++ Sources/include/ZCHeaders.h
@@ -0,0 +1,25 @@
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wstrict-prototypes"
+
+#ifndef ZCHeaders_H
+#define ZCHeaders_H
+
+#import "ZCAnimatedLabel.h"
+#import "ZCCoreTextLayout.h"
+#import "ZCDashLabel.h"
+#import "ZCDuangLabel.h"
+#import "ZCEasingUtil.h"
+#import "ZCEasingUtil.h"
+#import "ZCFallLabel.h"
+#import "ZCFlyinLabel.h"
+#import "ZCFocusLabel.h"
+#import "ZCRevealLabel.h"
+#import "ZCShapeshiftLabel.h"
+#import "ZCSpinLabel.h"
+#import "ZCThrownLabel.h"
+#import "ZCTransparencyLabel.h"
+
+#endif /* ZCHeaders_H */
+
+#pragma clang diagnostic pop
